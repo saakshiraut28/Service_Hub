c26d5956313026aab21e3115f66c6420fb4fe9e2
Saakshi Raut <saakshiraut28@gmail.com>
2024-03-17 19:35:53 +0530

    Modify: Home page Url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,20 +19,19 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-         <NavbarDefault /> 
+        <NavbarDefault />
         <Routes>
-          <Route path="/" element={<Home />} exact />
+          <Route path="/" element={<HomePageBL />} exact />
           <Route path="/about" element={<About />} />
           <Route path="/customer" element={<LandingPage />} />
           <Route path="/servicepage" element={<ServicePage />} />
           <Route path="/provider" element={<CustomerProviderProfile />} />
-          <Route path="/HomePageBL" element={<HomePageBL />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/ProviderDashboard" element={<ProviderDashboard />} />
           <Route path="/RatingPage" element={<RatingPage />} />
         </Routes>
-         <Footer /> 
+        <Footer />
       </BrowserRouter>
     </div>
   );
